e37a3f111186e4022c9c1fe9e89f82cb6e2df7a2
Hazar Deniz <95967990+HazarK21@users.noreply.github.com>
2023-05-04 00:59:45 +0300

    Update TSP.java

--- TSP.java
+++ TSP.java
@@ -7,9 +7,9 @@ public class TSP {
     private int numNodes;
     private ArrayList<Node> nodes;
 
-    public TSP(String dosyaAdi) {
+    public TSP(String fileName) {
         try {
-            BufferedReader br = new BufferedReader(new FileReader(dosyaAdi));
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
             String line = br.readLine();
             this.numNodes = Integer.parseInt(line);
             this.nodes = new ArrayList<Node>(this.numNodes);
@@ -26,12 +26,12 @@ public class TSP {
         }
     }
 
-    public void calistir() {
-        ArrayList<Integer> tur = enYakin();
-        ArrayList<Integer> optimalSira = new ArrayList<Integer>(tur);
-        double optimalMaliyet = maliyetHesapla(optimalSira);
-        System.out.println("Optimal cozum sirasi: " + optimalSira);
-        System.out.println("Optimal Maliyet: " + optimalMaliyet);
+    public void run() {
+        ArrayList<Integer> tour = nearest();
+        ArrayList<Integer> optimalOrder = new ArrayList<Integer>(tour);
+        double optimalCost = calculateCost(optimalOrder);
+        System.out.println("Optimal order: " + optimalOrder);
+        System.out.println("Optimal Cost: " + optimalCost);
     }
 
           private class Node {
@@ -44,65 +44,65 @@ public class TSP {
         }
     }
     
-    private ArrayList<Integer> enYakin() {
-        ArrayList<Integer> tur = new ArrayList<Integer>();
-        boolean[] ugra = new boolean[this.numNodes];
-        int mevcutNode = 0;
-        ugra[mevcutNode] = true;
-        tur.add(mevcutNode);
-        while (tur.size() < this.numNodes) {
-            double minMesafe = Double.MAX_VALUE;
-            int enYakinNode = -1;
+    private ArrayList<Integer> nearest() {
+        ArrayList<Integer> tour = new ArrayList<Integer>();
+        boolean[] visit = new boolean[this.numNodes];
+        int currentNode = 0;
+        visit[currentNode] = true;
+        tour.add(currentNode);
+        while (tour.size() < this.numNodes) {
+            double minDistance = Double.MAX_VALUE;
+            int nearestNode = -1;
             for (int i = 0; i < this.numNodes; i++) {
-                if (!ugra[i]) {
-                    double M = mesafe(this.nodes.get(mevcutNode), this.nodes.get(i));
-                    if (M < minMesafe) {
-                        minMesafe = M;
-                        enYakinNode = i;
+                if (!visit[i]) {
+                    double M = distance(this.nodes.get(currentNode), this.nodes.get(i));
+                    if (M < minDistance) {
+                        minDistance = M;
+                        nearestNode = i;
                     }
                 }
             }
-            ugra[enYakinNode] = true;
-            tur.add(enYakinNode);
-            mevcutNode = enYakinNode;
+            visit[nearestNode] = true;
+            tour.add(nearestNode);
+            currentNode = nearestNode;
         }
-        return tur;
+        return tour;
     }
 
-       private double mesafe(Node n1, Node n2) {
+       private double distance(Node n1, Node n2) {
         double xdiff = n1.x - n2.x;
         double ydiff = n1.y - n2.y;
         double mesf = Math.sqrt(xdiff * xdiff + ydiff * ydiff);
         return mesf;
     }
     
-    private double maliyetHesapla(ArrayList<Integer> tur) {
-        double maliyet = 0;
-        for (int i = 0; i < tur.size() - 1; i++) {
-            int node1 = tur.get(i);
-            int node2 = tur.get(i + 1);
+    private double calculateCost(ArrayList<Integer> tour) {
+        double cost = 0;
+        for (int i = 0; i < tour.size() - 1; i++) {
+            int node1 = tour.get(i);
+            int node2 = tour.get(i + 1);
             Node n1 = this.nodes.get(node1);
             Node n2 = this.nodes.get(node2);
-            maliyet += mesafe(n1, n2);
+            cost += distance(n1, n2);
         }
-        int ilkIndex = tur.get(0);
-        int sonIndex = tur.get(tur.size() - 1);
-        Node ilkNode = this.nodes.get(ilkIndex);
-        Node sonNode = this.nodes.get(sonIndex);
-        maliyet += mesafe(sonNode, ilkNode);
-        return maliyet;
+        int firstIndex = tour.get(0);
+        int lastIndex = tour.get(tour.size() - 1);
+        Node firstNode = this.nodes.get(firstIndex);
+        Node lastNode = this.nodes.get(lastIndex);
+        cost += distance(lastNode, firstNode);
+        return cost;
     }
 
     public static void main(String[] args) {
-        String[] dosyalar = {"tsp_5.txt", "tsp_124.txt", "tsp_1000.txt", "tsp_5915.txt", "tsp_11849.txt", "tsp_85900.txt"};
-        for (String dosyaAdi : dosyalar) {
+        String[] files = {"tsp_5.txt", "tsp_124.txt", "tsp_1000.txt", "tsp_5915.txt", "tsp_11849.txt", "tsp_85900.txt"};
+        for (String fileName : files) {
         long start = System.currentTimeMillis();
-        System.out.println("" + dosyaAdi +" "+ "Dosyasi isleniyor...");
-        TSP tsp = new TSP(dosyaAdi);
-        tsp.calistir();
+        System.out.println("" + fileName +" "+ "Reading file...");
+        TSP tsp = new TSP(fileName);
+        tsp.run();
         long finish = System.currentTimeMillis();
         long timeElapsed = finish - start;
-        System.out.println("Harcanan sure(ms)"+" "+ timeElapsed);
+        System.out.println("Time elapsed(ms)"+" "+ timeElapsed);
         System.out.println();
         }
         }
